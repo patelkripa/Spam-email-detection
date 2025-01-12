ee4d29c1d12364c6cd57b705793fd21d639bf1a0
Kripa Patel <patelkripa408@gmail.com>
2025-01-12 09:34:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Emial_Spam_Classification
+
+A Machine Learning project that detects whether the Email/SMS is SPAM or HAM.
+Technologies used: Python, Streamlit API
+Tools used: Jupyter Notebook, Excel
---
+++ spamDetector.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from win32com.client import Dispatch
+import pandas as pd
+import pythoncom  # Added pythoncom for COM initialization
+
+
+def speak(text):
+    pythoncom.CoInitialize()  # Initialize COM
+    try:
+        speaker = Dispatch("SAPI.SpVoice")
+        speaker.Speak(text)
+    finally:
+        pythoncom.CoUninitialize()  # Clean up COM
+
+
+# Load the model and vectorizer
+model = pickle.load(open('spam.pkl', 'rb'))
+cv = pickle.load(open('vectorizer.pkl', 'rb'))
+
+def main():
+    st.title(":green[Email Spam Detection]")
+    
+    # Sidebar options
+    activities = ["Classification", "About"]
+    choices = st.sidebar.selectbox("Select Activities", activities)
+    
+    # Classification functionality
+    if choices == "Classification":
+        st.subheader(":violet[Classification]")
+        msg = st.text_input("Enter a text")
+        if st.button("Process"):
+            data = [msg]
+            vec = cv.transform(data).toarray()
+            result = model.predict(vec)
+            
+            # Check if it's spam or not
+            if result[0] == 0:
+                st.success("This is Not A Spam Email", icon="✅")
+                speak("This is Not A Spam Email")
+                st.metric(label="HAM", value="95% SAFE", delta="GOOD")
+                
+            else:
+                st.error("This is A Spam Email", icon="🚨")
+                speak("This is A Spam Email")
+                st.metric(label="SPAM", value="-25% SAFE", delta="-BAD")
+    
+   
+        
+# Run the main function
+if __name__ == "__main__":
+    main()
